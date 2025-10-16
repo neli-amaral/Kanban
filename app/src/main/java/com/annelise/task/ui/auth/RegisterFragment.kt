4ab992cb8a6cca651e98f72fb4ed179114d64fa3
neli-amaral <anneliseamaraal@gmail.com>
2025-10-16 09:01:31 -0300

    integração com banco de dados cadastro de usuário

--- app/src/main/java/com/annelise/task/ui/auth/RegisterFragment.kt
+++ app/src/main/java/com/annelise/task/ui/auth/RegisterFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.view.isVisible
 import androidx.navigation.fragment.findNavController
 import com.annelise.task.R
 import com.annelise.task.databinding.FragmentRegisterBinding
@@ -24,7 +26,7 @@ class RegisterFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        // Inflate the layout for this fragment
+
         _binding = FragmentRegisterBinding.inflate(inflater, container, false)
         return binding.root
     }
@@ -46,12 +48,35 @@ class RegisterFragment : Fragment() {
         val senha = binding.editTextSenha.text.toString().trim()
         if(email.isNotBlank()) {
             if (senha.isNotBlank()) {
+                binding.progressbar.isVisible=true
+                registerUser(email, senha)
                 findNavController().navigate(R.id.action_global_homeFragment)
             } else {
                 showBottomSheet(message = getString(R.string.password_empty_register_fragment))
             }
         }else{
             showBottomSheet(message = getString(R.string.email_empty_register_fragment))
+        }
+    }
+
+
+    private fun registerUser(email: String, password: String){
+        try {
+            val auth = FirebaseAuth.getInstance()
+
+            auth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener { task ->
+                    if (task.isSuccessful){
+                        //mensagem de sucesso para cadastro do usuário
+                        findNavController().navigate(R.id.action_global_homeFragment)
+                    }else{
+                        //mensagem de erro para cadastro do usuário
+                        Toast.makeText(requireContext(), task.exception?.message, Toast.LENGTH_SHORT).show()
+                    }
+                }
+
+        }catch (e: Exception){
+            Toast.makeText(requireContext(), e.message.toString(), Toast.LENGTH_SHORT).show()
         }
     }
 
