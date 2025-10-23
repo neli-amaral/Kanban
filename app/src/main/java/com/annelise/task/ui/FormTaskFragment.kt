dc4e8c7ddd841a2a878fae36a7c05c4630787289
neli-amaral <anneliseamaraal@gmail.com>
2025-10-23 08:57:25 -0300

    finalização da atividade, colocando infos no banco de dados

--- app/src/main/java/com/annelise/task/ui/FormTaskFragment.kt
+++ app/src/main/java/com/annelise/task/ui/FormTaskFragment.kt
@@ -6,21 +6,42 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.core.view.isVisible
+import androidx.navigation.fragment.findNavController
 import com.annelise.task.R
+import com.annelise.task.data.model.Status
+import com.annelise.task.data.model.Task
 import com.annelise.task.databinding.FragmentFormTaskBinding
 import com.annelise.task.util.initToolbar
 import com.annelise.task.util.showBottomSheet
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.database
 
 class FormTaskFragment : Fragment() {
     private var _binding: FragmentFormTaskBinding? = null
     private val binding get() = _binding!!
+
+    private lateinit var task: Task
+
+    private var newTask: Boolean = true
+
+    private var status: Status = Status.TODO
+
+    //Banco de dados
+    private lateinit var reference: DatabaseReference
+
+    //Acesso ao serviço autentication
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        // Inflate the layout for this fragment
+
         _binding = FragmentFormTaskBinding.inflate(inflater, container, false)
         return binding.root
     }
@@ -28,18 +49,51 @@ class FormTaskFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         initToolbar(binding.toolbar)
+
+        //Instanciando banco de dados e autenticação
+        reference = Firebase.database.reference
+        auth = Firebase.auth
+
         initListener()
+    }
+
+    private fun saveTask(){
+        reference
+            .child("task")
+            .child(auth.currentUser?.uid ?: "")
+            .child(task.id)
+            .setValue(task).addOnCompleteListener { result ->
+                if (result.isSuccessful){
+                    Toast.makeText(requireContext(), R.string.text_save_sucess_task_fragment, Toast.LENGTH_SHORT).show()
+
+                    if (newTask){
+                        findNavController().popBackStack()
+                    }else{
+                        binding.progressBar.isVisible = false
+                    }
+                }else{
+                    binding.progressBar.isVisible = false
+                    showBottomSheet(message = getString(R.string.error_generic))
+                }
+
+            }
     }
     private fun initListener(){
         binding.buttonSave.setOnClickListener {
-            valideData()
+            validateData()
         }
     }
 
-    private fun valideData(){
+    private fun validateData(){
         val description = binding.editTextDescricao.text.toString().trim()
         if (description.isNotBlank()){
-            Toast.makeText(requireContext(),"Tudo OK!", Toast.LENGTH_SHORT).show()
+
+            if (newTask) task = Task(" ", " ", status )
+            task.id = reference.database.reference.push().key ?: ""
+            task.description = description
+            task.status = status
+
+            saveTask()
         }else{
             showBottomSheet(message = getString(R.string.description_empty_form_task_fragment))
         }
